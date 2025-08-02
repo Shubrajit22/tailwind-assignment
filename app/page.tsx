421ed6a4bd143b2fac768da93a6afbc05b25ac07
Shubrajit <shubrajitdeb180603@gmail.com>
2025-08-02 14:57:08 +0530

    completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,154 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="max-w-6xl h-screen bg-linear-to-b from-neutral-800 to-neutral-900  mx-auto flex  ">
+    <div className="w-4xl h-100 bg-neutral-600 rounded-xl  mx-auto my-100 perspective-[1000px]  ">
+     <div className="perspective-[1000px] flex flex-wrap">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  <button className="ml-2 bg-neutral-900 text-neutral-100 w-[100px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+    cursor-pointer text-left px-2 py-6
+    transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+    <span className="w-full">esc</span>
+  </button>
+
+  
+  {["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"].map((label, i) => (
+    <button
+      key={i}
+      className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+        cursor-pointer transform transition-all duration-300 ease-out hover:scale-95  hover:shadow-neutral-600"
+    >
+      {label}
+    </button>
+  ))}
+   {["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="].map((label, i) => (
+    <button
+      key={i}
+      className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+        cursor-pointer transform transition-all duration-300 ease-out hover:scale-95  hover:shadow-neutral-600"
+    >
+      {label}
+    </button>
+    
+  ))}
+  <button className="ml-2 bg-neutral-900 text-neutral-100 w-[100px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+    cursor-pointer text-right  py-6 px-1
+    transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+    <span className="w-full">delete</span>
+  </button>
+  <button className="ml-2 bg-neutral-900 text-neutral-100 w-[100px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+  cursor-pointer text-left px-2 py-6
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  <span className="w-full">tab</span>
+</button>
+
+{["Q", "W", "E", "R", "T", "V", "U", "I", "O", "P", "[", "]",].map((label, i) => (
+  <button
+    key={i}
+    className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+      cursor-pointer transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600"
+  >
+    {label}
+  </button>
+))}
+<button className="ml-2 bg-neutral-900 text-sm text-neutral-100 w-[100px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+  cursor-pointer text-left px-3 py-6 *:
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  caps lock
+</button>
+
+{["A", "S", "D", "F", "G", "H", "J", "K", "L", ":", "'", "enter"].map((label, i) => (
+  <button
+    key={i}
+    className={`ml-1 bg-neutral-900 text-neutral-100 ${label === "enter" ? "w-[120px] text-right py-6 px-2" : "w-[56px]"} h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400   
+      cursor-pointer transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600`}
+  >
+    {label}
+  </button>
+))}
+
+<button className="ml-2 bg-neutral-900 text-neutral-100 w-[120px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+  cursor-pointer text-left px-3
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  shift
+</button>
+
+{["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"].map((label, i) => (
+  <button
+    key={i}
+    className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+      cursor-pointer transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600"
+  >
+    {label}
+  </button>
+))}
+
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[120px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400  
+  cursor-pointer text-left px-3
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  shift
+</button>
+
+<button className="ml-2 bg-neutral-900 text-neutral-100 w-[80px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  ctrl
+</button>
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  fn
+</button>
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  ⌘
+</button>
+
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  alt
+</button>
+
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[300px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  space
+</button>
+
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  alt
+</button>
+
+
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[80px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  ctrl
+</button>
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[80px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  option
+</button>
+<button className="ml-1 bg-neutral-900 text-neutral-100 w-[60px] h-14 mt-2 rounded-xl shadow-2xl shadow-neutral-100 border border-neutral-400 
+   cursor-pointer text-center
+  transform transition-all duration-300 ease-out hover:scale-95 hover:shadow-neutral-600">
+  fn
+</button>
+
+</div>
+
     </div>
-  );
+    </div>
+  
+  )
 }
